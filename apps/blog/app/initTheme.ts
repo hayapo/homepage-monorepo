9b0e18a7bc85d8b7131af6b61e406e6cd5ff0283
hayapo <39045817+hayapo@users.noreply.github.com>
2026-01-01 21:21:13 +0900

    chore:テーマ変更機能はとりあえずやめた

--- apps/blog/app/initTheme.ts
+++ apps/blog/app/initTheme.ts
@@ -1,27 +1,14 @@
-const mediaQueryListener = (e: MediaQueryListEvent) => {
-	if (localStorage.theme === "system") {
+(() => {
+	if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+		document.documentElement.classList.add("dark");
+	}
+})();
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", (e: MediaQueryListEvent) => {
 		if (e.matches) {
 			document.documentElement.classList.add("dark");
 		} else {
 			document.documentElement.classList.remove("dark");
 		}
-	}
-};
-
-const initTheme = () => {
-	if (!("theme" in localStorage) || localStorage.theme === "system") {
-		if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-			document.documentElement.classList.add("dark");
-		}
-		localStorage.theme = "system";
-	} else if (localStorage.theme === "dark") {
-		document.documentElement.classList.add("dark");
-	} else {
-		document.documentElement.classList.remove("dark");
-	}
-};
-
-initTheme();
-window
-	.matchMedia("(prefers-color-scheme: dark)")
-	.addEventListener("change", mediaQueryListener);
+	});
