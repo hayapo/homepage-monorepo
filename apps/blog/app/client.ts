6425714c1d1a2d09ed79d189ece0286aadd8ff83
hayapo <39045817+hayapo@users.noreply.github.com>
2026-03-28 04:04:09 +0900

    iniializeHeaderとHeaderを@repo/uiからきりはなす

--- apps/blog/app/client.ts
+++ apps/blog/app/client.ts
@@ -1,5 +1,3 @@
-import { initializeHeader } from "@repo/ui";
 import { createClient } from "honox/client";
 
-initializeHeader();
 createClient();
--- apps/blog/app/components/Header/$Header.tsx
+++ apps/blog/app/components/Header/$Header.tsx
@@ -1,29 +1,9 @@
-import { HEADER_MOUNT_EVENT, HEADER_UNMOUNT_EVENT } from "@repo/ui/constants";
-import { useEffect, useId } from "hono/jsx";
-
 export const Header = () => {
-	const uniqueId = useId();
-	useEffect(() => {
-		const element = document.getElementById(uniqueId);
-		if (!element) {
-			return;
-		}
-		document.dispatchEvent(
-			new CustomEvent(HEADER_MOUNT_EVENT, { detail: { id: uniqueId } }),
-		);
-
-		return () =>
-			document.dispatchEvent(
-				new CustomEvent(HEADER_UNMOUNT_EVENT, { detail: { id: uniqueId } }),
-			);
-	}, [uniqueId]);
-	return (
-		<header class="h-(--header-height)" id={uniqueId}>
-			<div class="h-(--header-height) max-w-(--max-width) mx-auto flex justify-between items-center">
-				<div class="text-(length:--header-font-size) font-(--header-font-weight)">
-					hayapo.dev
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header class="h-(--header-height)">
+      <div class="h-(--header-height) max-w-(--max-width) mx-auto flex justify-between items-center">
+        <div class="text-(length:--header-font-size) font-(--header-font-weight)">hayapo.dev</div>
+      </div>
+    </header>
+  );
 };
