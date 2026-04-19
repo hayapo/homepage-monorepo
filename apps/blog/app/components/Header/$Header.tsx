64c607f2531f4147e4c4a8594e1eb0ff969c6a91
hayapo <39045817+hayapo@users.noreply.github.com>
2026-04-19 16:47:14 +0900

    ヘッダータイトルをブログトップページへのリンクに変更

--- apps/blog/app/components/Header/$Header.tsx
+++ apps/blog/app/components/Header/$Header.tsx
@@ -2,7 +2,9 @@ export const Header = () => {
   return (
     <header class="h-(--header-height)">
       <div class="h-(--header-height) max-w-(--max-width) mx-auto flex justify-between items-center">
-        <div class="text-(length:--header-font-size) font-(--header-font-weight)">hayapo.dev</div>
+        <a href="/" class="text-(length:--header-font-size) font-(--header-font-weight) hover:underline">
+          hayapo.dev
+        </a>
       </div>
     </header>
   );
