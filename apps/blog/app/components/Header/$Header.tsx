696ac5999f8a64596685015eee126b8d4bef4426
hayapo <39045817+hayapo@users.noreply.github.com>
2026-04-20 01:22:14 +0900

    ブログタイトルを変更&トップページではリンクを外す

--- apps/blog/app/components/Header/$Header.tsx
+++ apps/blog/app/components/Header/$Header.tsx
@@ -1,10 +1,22 @@
+import { useRequestContext } from "hono/jsx-renderer";
+
 export const Header = () => {
+  const blogTitle = "hayapo.log()";
+  const c = useRequestContext();
+  const isHome = c.req.path === "/";
   return (
     <header class="h-(--header-height)">
       <div class="h-(--header-height) max-w-(--max-width) mx-auto flex justify-between items-center">
-        <a href="/" class="text-(length:--header-font-size) font-(--header-font-weight) hover:underline">
-          hayapo.dev
-        </a>
+        {isHome ? (
+          <h1 class="text-(length:--header-font-size) font-(--header-font-weight) font-mono">{blogTitle}</h1>
+        ) : (
+          <a
+            href="/"
+            class="text-(length:--header-font-size) font-(--header-font-weight) hover:bg-green-a6 rounded-sm font-mono"
+          >
+            {blogTitle}
+          </a>
+        )}
       </div>
     </header>
   );
